ee956cd48e04e68a3988814ce5eff33a69c464a7
elkhailihamza <elkhailihamza331@gmail.com>
2025-02-26 10:06:32 +0000

    added the ability to fetch stock from product

--- e-store/stock-service/src/main/java/org/project/stockservice/dto/mapper/ProduitMapper.java
+++ e-store/stock-service/src/main/java/org/project/stockservice/dto/mapper/ProduitMapper.java
@@ -2,15 +2,24 @@ package org.project.stockservice.dto.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.project.stockservice.dto.ProduitDTO;
 import org.project.stockservice.entity.Produit;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {StockMapper.class})
 public interface ProduitMapper {
 
-    @Mapping(source = "categorie.id", target = "categorieId")
+    @Mappings({
+            @Mapping(source = "categorie.id", target = "categorieId"),
+            @Mapping(source = "stock", target = "stockDTO"),
+            @Mapping(target = "stockDTO.produitDTO", ignore = true)
+    })
     ProduitDTO toDTO(Produit produit);
 
-    @Mapping(source = "categorieId", target = "categorie.id")
+    @Mappings({
+            @Mapping(source = "categorieId", target = "categorie.id"),
+            @Mapping(source = "stockDTO", target = "stock"),
+            @Mapping(target = "stock.produit", ignore = true)
+    })
     Produit toEntity(ProduitDTO produitDTO);
 }
